e84851ee015c16a2cbbe3327851bef3741830fd0
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2021-12-03 14:52:33 +0000

    modify the way projects are displayed. this change includes introducing new styled components as well as modifying  already existing stled components. the changes introduced also includes an addition of three new constants.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,26 +1,70 @@
 import React from 'react';
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img, Container_wrapper, Container_flex, Container_Text, ContainerImg_Link, Container_H3, Container_P, Tagscontainer, ContainerImg, Tag_text, Button, Container_Text_right, } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import { projects, Project1, Project2, Project3} from '../../constants/constants';
 
 const Projects = () => (
-  <Section nopadding id="Projects">
-    <SectionDivider />
-    <SectionTitle main>Projects</SectionTitle>
-    <GridContainer>
-      {projects.map(({id, image, title, description, tags, source, visit }) => (
-        <BlogCard key={id}>
-          <Img src={image} />
-          <TitleContent>
-          <HeaderThree title>{title}</HeaderThree>
-          </TitleContent>
-          <Hr />
-          <CardInfo>{description}</CardInfo>
-        </BlogCard>
-      ))}
-    </GridContainer>
-  </Section>
+  <Container_wrapper id="Project1">
+        {Project1.map(({id, image, title, description, tags, source, visit }) => (
+          <Container_flex key={id}>
+            <Container_Text>
+              <Container_H3>{title}</Container_H3>
+              <Container_P>{description}</Container_P>
+                <Tagscontainer>
+                  <Tag_text>Product Design</Tag_text>
+                  <Tag_text>UX Design </Tag_text>
+                </Tagscontainer>
+              <a href="/">
+               <Button>View Case Study</Button>
+              </a>
+            </Container_Text>
+            <ContainerImg_Link href="/">
+              <ContainerImg src={image}/>
+            </ContainerImg_Link>
+          </Container_flex>
+        ))}
+        {Project2.map(({id, image, title, description }) => (
+          <Container_flex key={id}>
+            <ContainerImg_Link href="/">
+              <ContainerImg src={image}/>
+            </ContainerImg_Link>
+            <Container_Text_right>
+              <Container_H3>{title}</Container_H3>
+              <Container_P>{description}</Container_P>
+              <Tagscontainer>
+                <Tag_text>Rapid prototyping</Tag_text>
+                <Tag_text>UI Design</Tag_text>
+              </Tagscontainer>
+              <a href="/">
+               <Button>View Case Study</Button>
+              </a>
+            </Container_Text_right>
+          </Container_flex>
+
+        ))}
+
+        {Project3.map(({id, image, title, description }) => (
+          <Container_flex key={id}>
+            <Container_Text>
+              <Container_H3>{title}</Container_H3>
+                <Container_P>{description}</Container_P>
+                  <Tagscontainer>
+                    <Tag_text>Product Design</Tag_text>
+                    <Tag_text>UX Design </Tag_text>
+                  </Tagscontainer>
+                  <a href="/">
+                   <Button>View Case Study</Button>
+                  </a>
+            </Container_Text>
+            <ContainerImg_Link href="/">
+              <ContainerImg src={image}/>
+            </ContainerImg_Link>
+          </Container_flex>
+
+          ))}
+  </Container_wrapper>
+
 );
 
 export default Projects;
--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -9,10 +9,18 @@ export const Img = styled.img`
 // autry inspired
 
 // container wrapper
-export const Container_wrapper = styled.div`
+export const Container_wrapper = styled.section`
 width: 90%;
 margin: 0 auto;
+margin-top: 50px;
 max-width: 1100px;
+@media ${(props) => props.theme.breakpoints.sm} {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    padding-bottom: 0;
+  }
 `;
 export const Container_flex = styled.div`
   display: -webkit-box;
@@ -20,20 +28,42 @@ export const Container_flex = styled.div`
   display: flex;
   z-index: 100;
   position: relative;
-  -webkit-box-pack: justify;
+  /* -webkit-box-pack: justify; */
   /* -ms-flex-pack: justify; */
-  justify-content: space-between
+  justify-content: space-between!important;
+  margin-bottom: 400px;
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    width: 100%;
+    display: normal;
+    flex-direction: column;
+    padding: 2rem;
+    padding-bottom: 0;
+  }
 
 `;
 
 export const Container_Text = styled.div`
   position: relative;
+  justify-items: left;
+  width: 35%;
+  @media ${(props) => props.theme.breakpoints.sm}{
+    width: 100%;
+  }
+`;
+
+export const Container_Text_right = styled.div`
+  position: relative;
+  justify-items: right;
+  width: 35%;
+  @media ${(props) => props.theme.breakpoints.sm}{
+    width: 100%;
 `;
 // contents for container text.
 export const Container_H3 = styled.h3`
   display: block;
-  /* font-size: 1.17em; */
-  font-size: 400px;
+  font-size: 2.17em;
+  /* font-size: 400px; */
   margin-block-start: 1em;
   margin-block-end: 1em;
   margin-inline-start: 0px;
@@ -59,52 +89,121 @@ export const Container_P = styled.p`
   font-weight: 300;
 `;
 export const Tagscontainer = styled.div`
-  padding-top: 20px;
-  -webkit-box-orient: horizontal!important;
-  -webkit-box-direction: normal!important;
-  -webkit-box-pack: start!important;
-  -ms-flex-pack: start!important;
-  justify-content: flex-start!important;
-  /* -ms-flex-direction: row!important; */
-  flex-direction: row!important;
-  -webkit-box-pack: start!important;
-  -ms-flex-pack: start!important;
-  justify-content: flex-start!important;
+display: flex;
+z-index: 100;
+position: relative;
+padding-top: 20px;
+-webkit-box-orient: horizontal!important;
+-webkit-box-direction: normal!important;
+-webkit-box-pack: start!important;
+justify-content: flex-start!important;
 `;
 
 export const Tag_text = styled.h5`
-display: block;
-/* font-size: 0.83em; */
-margin-block-start: 1.67em;
-margin-block-end: 1.67em;
-margin-inline-start: 0px;
-margin-inline-end: 0px;
-/* font-weight: bold; */
-width: -webkit-fit-content;
-width: -moz-fit-content;
 width: fit-content;
 padding: 5px 10px;
 border-radius: 28px;
-margin-right: 10px;
+margin-right: 10px!important;
+margin: 10px;
+/* font-size: 0.83em; */
+color: black;
+/* margin-block-start: 1.67em;
+margin-block-end: 1.67em; */
+/* margin-inline-start: 0px; */
+/* margin-inline-end: 0px; */
+/* font-weight: bold; */
+/* width: -webkit-fit-content; */
+/* width: -moz-fit-content; */
+
 /* u */
 letter-spacing: .01em;
 font-weight: 300;
 font-size: 14px;
 background-color: #f7f7f7;
 margin: 0;
--webkit-box-direction: normal!important;
+/* -webkit-box-direction: normal!important; */
+`;
+export const Button = styled.div`
+  margin: 100px 0px 0px;
+  font-size: 18px;
+  text-align: center;
+  border-bottom: 3px solid #111;
+  position: relative;
+  -webkit-transition: all .25s;
+  -o-transition: all .25s;
+  transition: all .25s;
+  width: -webkit-fit-content;
+  width: -moz-fit-content;
+  width: fit-content;
+  padding: 20px 0;
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  -webkit-box-shadow: 0 0 1px transparent;
+  box-shadow: 0 0 1px transparent;
+  position: relative;
+  -webkit-transition-property: color;
+  -o-transition-property: color;
+  transition-property: color;
+  -webkit-transition-duration: .25s;
+  -o-transition-duration: .25s;
+  transition-duration: .25s;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    margin: 20px 0;
+  }
 `;
 
+export const ButtonHvr = styled.div`
+  content: "";
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #111;
+  --webkit-transform: scaleY(0);
+  --ms-transform: scaleY(0);
+  transform: scaleY(0);
+  -webkit-transform-origin: 50% 100%;
+  --ms-transform-origin: 50% 100%;
+  transform-origin: 50% 100%;
+  --webkit-transition-property: transform;
+  --webkit-transition-property: -webkit-transform;
+  transition-property: -webkit-transform;
+  --o-transition-property: transform;
+  transition-property: transform;
+  transition-property: transform,-webkit-transform;
+  --webkit-transition-duration: .25s;
+  --o-transition-duration: .25s;
+  transition-duration: .25s;
+  --webkit-transition-timing-function: ease-out;
+  --o-transition-timing-function: ease-out;
+  transition-timing-function: ease-out;
+`;
 export const ContainerImg_Link = styled.a`
-
+  width: 55%;
+  display: block;
+  cursor: pointer;
+  transition: all .25s;
+  text-decoration: none;
+  @media ${(props) => props.theme.breakpoints.sm}{
+    width: 100%;
+  }
 `;
 export const ContainerImg = styled.img`
-
+width: 100%;
+height: 100%;
+--webkit-animation-delay: .25s!important;
+animation-delay: .25s!important;
+--webkit-transition: all .65s;
+--o-transition: all .65s;
+transition: all .65s;
+border-radius: 5px;
+box-shadow: 0 10px 60px 0 rgb(0 0 0 / 20%);
 `;
 
-export const ContainerText = styled.a`
-
-`;
 
 export const GridContainer = styled.section`
 display: grid;
