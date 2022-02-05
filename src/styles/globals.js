dc214139512947f6f7808acde75f8cc8ab18f676
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2022-02-05 08:42:04 +0000

    Modify main font for pages

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -14,13 +14,14 @@ const GlobalStyles = createGlobalStyle`
     scroll-behavior: smooth;
 
   }
-  body {
+  body, #__next {
     font-family: ${props => props.theme.fonts.main};
     font-size: 1.6rem;
     background: ${props => props.theme.colors.background1};
     color: ${props => props.theme.colors.primary1};
     cursor: default;
     width: 100%;
+    height: 100%
 
   }
   h1,h2,h3,h4,h5,h6,button {
