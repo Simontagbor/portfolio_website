716c0c51a561fbd20525e758b5be5906f286f792
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2022-02-05 08:51:00 +0000

    specify height of new pages

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -14,14 +14,13 @@ const GlobalStyles = createGlobalStyle`
     scroll-behavior: smooth;
 
   }
-  body, #__next {
+  body {
     font-family: ${props => props.theme.fonts.main};
     font-size: 1.6rem;
     background: ${props => props.theme.colors.background1};
     color: ${props => props.theme.colors.primary1};
     cursor: default;
     width: 100%;
-    height: 100%
 
   }
   h1,h2,h3,h4,h5,h6,button {
