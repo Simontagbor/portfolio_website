8c9edc63fd6ef266e725736794d18b592a25633d
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2022-02-19 23:10:51 +0000

    testing new components

--- src/components/chatbot/Chatbot.js
+++ src/components/chatbot/Chatbot.js
@@ -27,7 +27,21 @@ import {
   VectorImg2,
   Infocardtext,
   Heading,
-  Details
+  Details,
+  Overviewflex,
+  Quote,
+  RedFrame,
+  DetailCard,
+  IconRed,
+  H1_R,
+  Themetitle,
+  ThememarkerRed,
+  ProjectOverviewh1sm5,
+  RedThemeContainer,
+  InfocardGroup,
+
+
+
 } from "../../styles/GlobalComponents/CaseStudyStyles";
 
 const ChatbotCase = () => (
@@ -65,10 +79,12 @@ const ChatbotCase = () => (
       <ImgContainer src="images/projects.jpeg"></ImgContainer>
     </Mockup_flex1>
     <MacbookPro1441>
+      <Overviewflex>
       <Overviewlg>
         <Themelinelg />
         <ProjectOverviewlg>Project Overview</ProjectOverviewlg>
       </Overviewlg>
+      <InfocardGroup>
       <Infocardcontainer>
         <Infocard>
           <Icon>
@@ -87,6 +103,25 @@ const ChatbotCase = () => (
             </Details>
           </Infocardtext>
         </Infocard>
+        <Infocard>
+          <Icon>
+            <Database>
+              <Vectordiv>
+                <VectorImg src="https://storage.googleapis.com/rd-stg-designs/project/5eed9488-bb02-459f-a6b3-ce5857c05302/assets/d6cb997d-0667-4edb-bcf8-6fced6482e84.png" />
+                <VectorImg2 src="https://storage.googleapis.com/rd-stg-designs/project/5eed9488-bb02-459f-a6b3-ce5857c05302/assets/b80e9199-34f0-4455-b772-0e8196a1df07.png" />
+              </Vectordiv>
+            </Database>
+          </Icon>
+          <Infocardtext>
+            <Heading>Heading</Heading>
+            <Details>
+              Details will go here lorem ipsum kinda details if you know what i
+              mean. so we can alll agreee that this is becoming great
+            </Details>
+          </Infocardtext>
+        </Infocard>
+      </Infocardcontainer>
+      <Infocardcontainer>
         <Infocard>
           <Icon>
             <Database>
@@ -122,6 +157,98 @@ const ChatbotCase = () => (
           </Infocardtext>
         </Infocard>
       </Infocardcontainer>
+      </InfocardGroup>
+      </Overviewflex>
+      <TextContainer>
+      <H1>The Case Study </H1>
+      <Paragraph>
+        World Trade Center Accra is searching for an experienced event
+        coordinator to partner our organization in hosting Friday events that
+        enhance our brand-to-client experience. The event coordinator will be
+        tasked with planning and managing our events' calendar, negotiating
+        quotes and agreements with vendors, assisting with event promotion and
+        effectively delivering on event objectives.
+      </Paragraph>
+      <H2>Defining The Problem</H2>
+    </TextContainer>
+    <Mockup_flex1>
+      <ImgContainer src="images/projects.jpeg"></ImgContainer>
+      <TextBox>
+        <TextBoxH1>The feature</TextBoxH1>
+        <TextBoxP>
+          The event coordinator will be tasked with planning and managing our
+          events'
+        </TextBoxP>
+      </TextBox>
+    </Mockup_flex1>
+    <Mockup_flex1>
+      <TextBox>
+        <TextBoxH1>The feature</TextBoxH1>
+        <TextBoxP>
+          The event coordinator will be tasked with planning and managing our
+          events'
+        </TextBoxP>
+      </TextBox>
+      <ImgContainer src="images/projects.jpeg"></ImgContainer>
+    </Mockup_flex1>
+      <Overviewflex>
+      <Themetitle>
+        <ThememarkerRed />
+        <ProjectOverviewh1sm5>
+          Understanding the User
+        </ProjectOverviewh1sm5>
+      </Themetitle>
+      <RedThemeContainer>
+        <RedFrame>
+          <DetailCard>
+            <IconRed>
+              <Database>
+              </Database>
+            </IconRed>
+            <H1_R>
+              User Research
+            </H1_R>
+          </DetailCard>
+        </RedFrame>
+        <RedFrame>
+          <DetailCard>
+            <IconRed>
+              <Database>
+              </Database>
+            </IconRed>
+            <H1_R>
+              User Research
+            </H1_R>
+          </DetailCard>
+        </RedFrame>
+        <RedFrame>
+          <DetailCard>
+            <IconRed>
+              <Database>
+              </Database>
+            </IconRed>
+            <H1_R>
+              User Research
+            </H1_R>
+          </DetailCard>
+        </RedFrame>
+        <RedFrame>
+          <DetailCard>
+            <IconRed>
+              <Database>
+              </Database>
+            </IconRed>
+            <H1_R>
+              User Research
+            </H1_R>
+          </DetailCard>
+        </RedFrame>
+      </RedThemeContainer>
+      <Quote>
+        “some thoughtful quote about user centered design and or process”
+      </Quote>
+      </Overviewflex>
+
     </MacbookPro1441>
   </ContentBox>
 );
