f66225ca97e049b1fd73da85340ddd9d9128b882
Simontagbor <simontagbor360@gmail.com>
2022-03-19 14:46:36 +0000

    update layout previous update was not done correctly.

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import Footer from '../components/Footer/Footer';
-import Header from '../components/Header/Header';
-import Navbar from '../components/Navbar';
-import { Container } from './LayoutStyles';
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Navbar from "../components/Navbar";
+import { Container, MyContainer } from "./LayoutStyles";
 // import ChatbotCase from '../components/chatbot/Chatbot';
 
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
   return (
-    
-    <Container>
-     <div>
-      <Header/>
-     </div>
-     {/* <Navbar /> */}
-     <main>{children}</main>
-     {/* <ChatbotCase/> */}
-     {/* <Footer/> */}
-    </Container>
-  )
-}
+    <>
+      <Header />
+      <MyContainer>
+        <main>{children}</main>
+      </MyContainer>
+      {/* <main>{children}</main> */}
+      {/* <ChatbotCase/> */}
+      {/* <Footer/> */}
+      {/* </Container> */}
+    </>
+  );
+};
--- src/layout/LayoutStyles.js
+++ src/layout/LayoutStyles.js
@@ -1,7 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-max-width: 100%;
-width: 100%;
-margin: auto;
+  max-width: 100%;
+  width: 100%;
+  margin: auto;
+`;
+
+export const MyContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  max-width: 100%;
+
+  @media ${(props) => props.theme.breakpoints.lg} {
+    width: 55%;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    width: 65%;
+  }
+  @media ${(props) => props.theme.breakpoints.sm} {
+    width: 80%;
+  }
 `;
